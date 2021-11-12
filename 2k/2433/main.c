2099aafee380a834b752857ab61479146623715c
josedelinux <josedelinux@hotmail.com>
2021-11-12 20:42:16 +0800

    na

--- 2k/2433/main.c
+++ 2k/2433/main.c
@@ -1,21 +1,41 @@
-#include<stdio.h>
+#include <math.h>
+#include <stdio.h>
 
-int main(){
-    int T;
-    
-    scanf("%d",&T);
-    
-    switch (T)
+int
+main ()
+{
+  int T;
+  const double pi = acos (-1);
+  scanf ("%d", &T);
+
+  switch (T)
     {
     case 1:
-        printf("I love Luogu!");
-        break;
+      printf ("I love Luogu!\n");
+      break;
     case 2:
-        printf("%d %d",2+4, 10-2-4);
-        break;
+      printf ("%d %d\n", 2 + 4, 10 - 2 - 4);
+      break;
+    case 3:
+      printf ("%d\n%d\n%d\n", 14 / 4, 14 - 14 / 4);
+      break;
+    case 4:
+      printf ("%3f\n", 500 / 3.0);
+      break;
+    case 5:
+      printf ("%d\n", (220 + 260) / (20 + 12));
+      break;
+    case 6:
+      // cout<<sqrt(6*6+9*9);
+      printf ("10.8167\n");
+    case 7:
+      printf ("110\n90\n0");
+      break;
+    case 8:
+      printf ("%f\n%f\n%f\n", pi * 5 * 2, pi * 5 * 5, 4 / 3 * pi * 125);
     default:
-        break;
+      break;
     }
 
-    return 0;
+  return 0;
 }
