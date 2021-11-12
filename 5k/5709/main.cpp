48071e96ff8c70e038588a8ea1cd5b4efa4eb06e
josedelinux <josedelinux@hotmail.com>
2021-11-13 00:13:17 +0800

    na

---
+++ 5k/5709/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int
+main ()
+{
+  int m, t, s;
+  cin >> m >> t >> s;
+
+  if (t == 0)
+    {
+      cout << 0 << endl;
+      return 0;
+    }
+  if (s % t == 0)
+    {
+      cout << max (m - s / t, 0);
+    }
+  else
+    {
+      cout << max (m - s / t - 1, 0);
+    }
+  return 0;
+}
