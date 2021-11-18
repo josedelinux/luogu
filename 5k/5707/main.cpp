9c0899a03718b7dceeddee6109e8a54a375bb85a
josedelinux <josedelinux@hotmail.com>
2021-11-18 17:48:46 +0800

    Update main.cpp

--- 5k/5707/main.cpp
+++ 5k/5707/main.cpp
@@ -3,10 +3,24 @@
 using namespace std;
 
 int
+myceil (float num)
+{
+  int a = num;
+  if ((float)a != num)
+    return num + 1;
+  return num;
+}
+
+int
 main ()
 {
   int h, m, s, v;
   scanf ("%d%d", &s, &v);
 
-  printf ("%02d:%02d\n", h, m);
+  // int mdiff = (m2 + 120 - m1) % 60;
+  int mdiff = (10 + myceil (s / v));
+  m = (120 - mdiff) % 60;
+  int hdiff = (mdiff / 60) + 1;
+  h = 8;
+  printf ("%02d:%02d\n", h - hdiff, mdiff);
 }
