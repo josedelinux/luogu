1a4bf0b755b74471e941847fa2c4af3dc1704d0d
josedelinux <josedelinux@hotmail.com>
2021-12-10 21:13:42 +0800

    Create main.cpp

---
+++ 1k/1085/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int a, b, ans, max = 0;
+  for (int i = 0; i < 7; i++) {
+    scanf("%d %d", &a, &b);
+    int sum = a + b;
+    if (sum != max && sum >= 8) {
+      if (sum >= max) {
+        max = sum;
+        ans = i - 1;
+      }
+    }
+  }
+  if (max == 0) {
+    ans = 0;
+  }
+  printf("%d\n", ans);
+  return 0;
+}
