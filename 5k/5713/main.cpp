9e21f492f8718abd61e16d6ffd2209895d572558
josedelinux <josedelinux@hotmail.com>
2021-12-10 15:17:07 +0800

    Create main.cpp

---
+++ 5k/5713/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int
+main ()
+{
+  int num;
+  scanf ("%d", &num);
+  if (num > 5)
+    {
+      printf ("Luogu\n");
+    }
+  else
+    {
+      printf ("Local\n");
+    }
+
+  return 0;
+}
