0a9f7004489e1eb0b286e044f37dfeb2ab1c8b3e
josedelinux <josedelinux@hotmail.com>
2021-12-10 23:59:34 +0800

    Update main.cpp

---
+++ 1k/1055/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  char s[14];
+
+  // check digits
+  int given, m_check = 0;
+
+  scanf("%c-%c%c%c-%c%c%c%c%c-%c", &s[1], &s[2], &s[3], &s[4], &s[5], &s[6],
+        &s[7], &s[8], &s[9], &given);
+
+  for (int i = 1; i <= 9; i++) {
+    m_check = m_check + i * (s[i] - '0');
+  }
+  m_check = m_check % 11;
+  if (m_check == given - '0') {
+    printf("Right\n");
+  } else {
+    printf("%c-%c%c%c-%c%c%c%c%c-%d\n", s[1], s[2], s[3], s[4], s[5], s[6],
+           s[7], s[8], s[9], m_check);
+  }
+
+  return 0;
+}
