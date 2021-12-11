8d5126b8bfaed58b49e4f70a50dcf627b46e714d
josedelinux <josedelinux@hotmail.com>
2021-12-11 11:16:13 +0800

    na

--- 1k/1422/solution2.cpp
+++ 1k/1422/solution2.cpp
@@ -1,5 +1,7 @@
-// https://www.luogu.com.cn/blog/ql-luogu-org/solution-p1422
+/* https://www.luogu.com.cn/blog/ql-luogu-org/solution-p1422
+适合分段极多，写if容易混乱时使用
 
+*/
 #include <iostream>
 using namespace std;
 double cost[10005];  //代表第i度电的花费
@@ -12,6 +14,8 @@ int main() {
   for (i = 151; i <= 400; i++) cost[i] = 0.4663;
   for (i = 401; i <= 10000; i++) cost[i] = 0.5663;
   for (i = 1; i <= a; i++) b += cost[i];
+
+  // nice way to setprecision without std::setprecision(n)
   b = int((b * 10) + 0.5) / 10.0;
   cout << b << endl;
   return 0;
---
+++ 1k/1424/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x, n, ans;
+  scanf("%d%d", &x, &n);
+  printf("%d", ans);
+  return 0;
+}
