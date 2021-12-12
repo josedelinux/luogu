718dc3677e82d79eb67db134f3d8aedb16139ba4
josedelinux <josedelinux@hotmail.com>
2021-12-12 18:20:50 +0800

    Create main.cpp

---
+++ 4k/4414/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  // order, s[0] is zero
+  char s[4];
+  scanf("%d%d%d", &a, &b, &c);
+  scanf("%s", s[1]);
+
+  printf("%d %d %d\n", a, b, c);
+  return 0;
+}
