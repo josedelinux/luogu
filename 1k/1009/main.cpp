66c4bfc8fb4d5f50ba3f365f70430065c7a75d5b
josedelinux <josedelinux@hotmail.com>
2021-12-12 23:24:14 +0800

    Create main.cpp

---
+++ 1k/1009/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define maxn 51
+using namespace std;
+
+unsigned long long facArr[maxn];
+
+int main() {
+  int sum = 0;
+  int n;
+  scanf("%d", &n);
+
+  facArr[0] = 1;
+  facArr[1] = 1;
+
+  // generate all the factors
+  for (int i = 2; i <= maxn; i++) {
+    facArr[i] = i * facArr[i - 1];
+  }
+  printf("%d", sum);
+
+  return 0;
+}
