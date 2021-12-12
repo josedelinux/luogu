b8c291a60d774bc89ebe06bbc19be7701e6fa0e2
josedelinux <josedelinux@hotmail.com>
2021-12-12 18:37:55 +0800

    Update main.cpp

--- 4k/4414/main.cpp
+++ 4k/4414/main.cpp
@@ -2,12 +2,28 @@
 using namespace std;
 
 int main() {
+  // results
   int a, b, c;
+  // input array
+  int arr[4];
   // order, s[0] is zero
-  char s[4];
-  scanf("%d%d%d", &a, &b, &c);
-  scanf("%s", s[1]);
-
+  char order[4];
+  scanf("%d%d%d", arr + 1, arr + 2, arr + 3);
+  scanf("%s", order + 1);
+  sort(arr + 1, arr + 4);
+  for (int i = 1; i <= 3; i++) {
+    switch (order[i]) {
+      case 'A':
+        a = arr[i];
+        break;
+      case 'B':
+        b = arr[i];
+        break;
+      case 'C':
+        c = arr[i];
+        break;
+    }
+  }
   printf("%d %d %d\n", a, b, c);
   return 0;
 }
