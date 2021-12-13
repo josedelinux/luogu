aadb3257b8cc5def015f34e086b3c064210a23ed
josedelinux <josedelinux@hotmail.com>
2021-12-13 12:27:43 +0800

    Update gen.py

--- 1k/1009/gen.py
+++ 1k/1009/gen.py
@@ -1,17 +1,20 @@
-
 # answer string Generator
 
-
 f = open(r'point.txt', 'w')  # 存入文件
+f.write('"')  # 上引号
+# factorial 0 = 1
+f.write(str(1))  # 具体值
+f.write('",')  # 下引号与逗号
 sum = 1
 i = 2
 last = 1
 n = int(input())
 while i <= n:
-    last *= i
-    sum += last
-    i += 1  # 主函数
     f.write('"')  # 上引号
     f.write(str(sum))  # 具体值
     f.write('",')  # 下引号与逗号
+    last *= i
+    sum += last
+    i += 1  # 主函数
+
 f.close  # 关闭文件写入
