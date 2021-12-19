25b98602b21ac3b49ded7794ca9ab7f083818b99
josedelinux <josedelinux@hotmail.com>
2021-12-19 11:15:52 +0800

    Update main.cpp

--- 1k/1217/main.cpp
+++ 1k/1217/main.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 
+int is_prime(int n) {
+  int i;
+  for (i = 1; i < n; i++) {
+    if (n % i == 0) break;
+  }
+  if (i = n)
+    return 1;
+  else
+    return 0;
+}
 int main() {
   int a, b;
   scanf("%d%d", &a, &b);
-
+  for (int i = a; i < b; i++) {
+  }
   return 0;
 }
