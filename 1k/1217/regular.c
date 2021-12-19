8c81b014787347492c0d878d772588fcfca11f0c
josedelinux <josedelinux@hotmail.com>
2021-12-19 14:49:36 +0800

    Update regular.c

--- 1k/1217/regular.c
+++ 1k/1217/regular.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #define maxn 100000010
 
+int ans[maxn];
+
 // simplest, slow as fuck
 int is_prime_deprecated(int n) {
   int i;
@@ -13,9 +15,6 @@ int is_prime_deprecated(int n) {
     return 0;
 }
 
-// sieve of Eratosthenes
-int is_prime(int n) {}
-
 int is_palindrome(int n) {
   int y = n, num = 0;
   while (y != 0) {
@@ -26,18 +25,38 @@ int is_palindrome(int n) {
   return 0;
 }
 
-// int ans[maxn];
+// sieve of Eratosthenes
+void primeGen(int b) {
+  //初始化，默认全部都是质数
+  memset(ans, 1, sizeof(ans));
+  ans[0] = ans[1] = 0;
+  int n = sqrt(b);
 
-int main() {
-  int a, b;
-
-  scanf("%d%d", &a, &b);
-
-  for (int i = a; i < b; i++) {
-    if (is_palindrome(i) && is_prime(i)) {
-      printf("%d\n", i);
+  for (int i = 2; i <= n; i++) {
+    if (ans[i]) {
+      //质数的倍数绝对不是质数，把所有质数的倍数全部设为0
+      for (int j = 2; j <= b / i; j++) {
+        ans[i * j] = 0;  // i*j<=b}
+      }
     }
   }
 
-  return 0;
-}
+  int main() {
+    int a, b;
+    primeGen();
+    scanf("%d%d", &a, &b);
+
+    // b<=10000000这个判断条件来自：除了11以外，一个数的位数是偶数的话，不可能为回文数素数。
+    // 如果一个回文素数的位数是偶数，则它的奇数位上的数字和与偶数位上的数字和必然相等；
+    // 根据数的整除性理论，容易判断这样的数肯定能被11整除，所以它就不可能是素数。
+    if (b >= 10000000) b = 9999999;
+    primeGen(b);
+    if (a > b) return -1;
+    for (int i = a; i <= b; i++) {
+      if (ans[i] && is_palindrome(i)) {
+        printf("%d", i);
+      }
+    }
+
+    return 0;
+  }
