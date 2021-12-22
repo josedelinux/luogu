2459da0620f83a1206e5cdcb5291fba286205256
josedelinux <josedelinux@hotmail.com>
2021-12-22 23:23:05 +0800

    Update main.cpp

--- 1k/1423/main.cpp
+++ 1k/1423/main.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 int main() {
-  double x, sum = 2;
+  double x, sum = 0;
+  double aumencion = 2.0;
 
   int ans = 1;
   scanf("%lf", &x);
   while (sum < x) {
-    sum = sum + sum * 0.98;
+    aumencion = aumencion * 0.98;
+    sum = sum + aumencion;
     ans += 1;
   }
   printf("%d\n", ans);
