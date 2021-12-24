176d105604f4dcfd6d576f2d2ccfc894da851d7b
josedelinux <josedelinux@hotmail.com>
2021-12-24 22:32:40 +0800

    Update main.cpp

--- p/5k/5724/main.cpp
+++ p/5k/5724/main.cpp
@@ -1,22 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define infty 1000000
 
 using namespace std;
 
 int main()
 {
-	int n=0;
-	int ans=0;
+	int n = 0;
+	int ans = 0;
 	int max = -infty;
 	int min = infty;
-	scanf("%d",&n);
-	for(int i=0; i<n; i++) {
-		int tmp=0;
-		scanf("%d",&tmp);
-		if(tmp>max) max=tmp;
-		if(tmp<min) min =tmp;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int tmp = 0;
+		scanf("%d", &tmp);
+		if (tmp > max) max = tmp;
+		if (tmp < min) min = tmp;
 	}
-	ans =max -min;
-	printf("%d",ans);
+	ans = max - min;
+	printf("%d\n", ans);
 	return 0;
 }
