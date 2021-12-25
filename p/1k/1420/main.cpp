f7243886103426ca2c2a9126a8274f83b2c0d6b2
josedelinux <josedelinux@hotmail.com>
2021-12-25 10:19:46 +0800

    Update main.cpp

--- p/1k/1420/main.cpp
+++ p/1k/1420/main.cpp
@@ -9,19 +9,33 @@ int main()
 	int arr[1010] = {0};
 
 	scanf("%d", &n);
+
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &arr[i]);
 	}
+
+	//nao fue preciso sort
 	sort(arr, arr + n);
+
+	//get rid of repeat ones
+	std::map<int,int>cleaned;
+	for (int i = 0; i < n; i++) {
+		cleaned[arr[i]]=1;
+	}
+
+	//cleaned array
+	int a_c[n]= {0};
+	int tc=0; //tmp cnt
+	for (int i = 0; i < n; i++) {
+		if(cleaned[i]) {
+			//printf("%d ",i);
+			a_c[tc++]=i;
+		}
+	}
 
 	int tmp=0;
 	for (int i = 0; i < n; i++) {
-		int offset =1;
-		//adjust offset to skip same value
-		if(arr[i + offset] == arr[i] ) {
-			offset++;
-		}
-		if (arr[i + offset] - arr[i] == 1) {
+		if (a_c[i + 1] - a_c[i] == 1) {
 			tmp++;
 			ans=max(ans,tmp+1);
 		} else {
