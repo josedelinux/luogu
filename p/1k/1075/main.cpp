909e024df0c3fb46e751f6806cb102ea5613fafc
josedelinux <josedelinux@hotmail.com>
2021-12-26 16:18:18 +0800

    Update main.cpp

--- p/1k/1075/main.cpp
+++ p/1k/1075/main.cpp
@@ -1,3 +1,10 @@
+/*
+It's Known to all the factors to a number comes in pairs
+for example 21:
+1,21 3,7
+thus all we need to do is to find the smallest one to divide the number
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -8,6 +15,14 @@ int main()
 	int n;
 	int ans=0;
 	scanf("%d",&n);
+
+	//yes use sqrt here!!!
+	for(int i=2; i<sqrt(n); i++) {
+		if(n%i==0) {
+			ans= n/i;
+			break;
+		}
+	}
 
 	printf("%d\n",ans);
 	return 0;
