93f623bcb6096ee9a2cfef59bb4268be52d95f4f
josedelinux <josedelinux@hotmail.com>
2021-12-26 12:45:40 +0800

    Update main.cpp

--- p/1k/1420/main.cpp
+++ p/1k/1420/main.cpp
@@ -8,37 +8,19 @@ using namespace std;
 
 int main() {
   int n = 0;
-  int ans = 0;
+  int ans = 1;
   int arr[1010] = {0};
 
+  /* input */
   scanf("%d", &n);
 
   for (int i = 0; i < n; i++) {
     scanf("%d", &arr[i]);
   }
 
-  // nao fue preciso sort
-  sort(arr, arr + n);
-
-  // get rid of repeat ones
-  std::map<int, int> cleaned;
-  for (int i = 0; i < n; i++) {
-    cleaned[arr[i]] = 1;
-  }
-
-  // cleaned array
-  int a_c[n] = {0};
-  int tc = 0;  // tmp cnt
-  for (int i = 0; i < n; i++) {
-    if (cleaned[i]) {
-      // printf("%d ",i);
-      a_c[tc++] = i;
-    }
-  }
-
   int tmp = 0;
   for (int i = 0; i < n; i++) {
-    if (a_c[i + 1] - a_c[i] == 1) {
+    if (arr[i + 1] - arr[i] == 1) {
       tmp++;
       ans = max(ans, tmp + 1);
     } else {
@@ -46,6 +28,7 @@ int main() {
     }
   }
 
+  /* output */
   printf("%d\n", ans);
   return 0;
 }
