7476fbb02a7a30102e3fe63e5a8ca951a952c40d
josedelinux <josedelinux@hotmail.com>
2021-12-26 19:44:18 +0800

    Update main.cpp

--- p/4k/4956/main.cpp
+++ p/4k/4956/main.cpp
@@ -1,10 +1,14 @@
 /*
 N can be Divided by integer 52
-so we handle it by week
+so we handle it by week (obvious)
 let's go brutal
 Vamos brutal
 
 (7*x+21*k)*52==n
+
+since
+"and if there are multiple, output the one with the greatest X  and smallest K."
+let's loop x over from 100
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -13,7 +17,7 @@ int main()
 {
 
 	int n;
-	int x,k;
+	int x,k=1;
 	scanf("%d",&n);
 	while(1) {
 		for(x=100; x>0; x--) {
@@ -22,6 +26,7 @@ int main()
 				return 0;
 			}
 		}
+		//if there are no solution found
 		k++;
 	}
 	return 0;
