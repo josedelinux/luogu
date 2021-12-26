e71a2a4f25610906ac615c75794df3b05f2cc08c
josedelinux <josedelinux@hotmail.com>
2021-12-26 19:30:03 +0800

    Create main.cpp

---
+++ p/4k/4956/main.cpp
@@ -0,0 +1,18 @@
+/*
+N can be Divided by integer 52
+so we handle it by week
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+	int n;
+	int x,k;
+	scanf("%d",&n);
+
+	printf("%d\n",x);
+	printf("%d\n",k);
+	return 0;
+}
