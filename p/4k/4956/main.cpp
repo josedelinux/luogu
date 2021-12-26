55eb67b9ff34993cd80c4739ec17d696fa0dcbf5
josedelinux <josedelinux@hotmail.com>
2021-12-26 19:37:50 +0800

    Update main.cpp

--- p/4k/4956/main.cpp
+++ p/4k/4956/main.cpp
@@ -1,6 +1,10 @@
 /*
 N can be Divided by integer 52
 so we handle it by week
+let's go brutal
+Vamos brutal
+
+(7*x+21*k)*52==n
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -11,8 +15,14 @@ int main()
 	int n;
 	int x,k;
 	scanf("%d",&n);
-
-	printf("%d\n",x);
-	printf("%d\n",k);
+	while(1) {
+		for(x=100; x>0; x--) {
+			if((7*x+21*k)*52==n) {
+				printf("%d\n%d\n",x,k);
+				return 0;
+			}
+		}
+		k++;
+	}
 	return 0;
 }
