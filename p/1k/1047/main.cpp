d581e7334624f489c41f6c2ba075768eac58cf40
josedelinux <josedelinux@hotmail.com>
2021-12-29 23:11:45 +0800

    buggy

---
+++ p/1k/1047/main.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define maxn 10005
+
+char arr[maxn];
+
+int main()
+{
+	int ans=0;
+	//init to 0
+	memset(arr,0,maxn);
+	int l,m;
+	scanf("%d",&l);
+	memset(arr,1,l);
+
+	scanf("%d",&m);
+	while(m--) {
+		int a,b;
+		scanf("%d %d",&a,&b);
+		for(int i=a; i<=b; i++) {
+			arr[i]=0;
+		}
+	}
+	
+	
+	for(int i=0; i<=l; i++) {
+		if(arr[i]) ans++;
+	}
+	printf("%d\n",ans);
+	return 0;
+}
