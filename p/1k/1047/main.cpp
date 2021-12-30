51bc8ace0a2ca00980a4a10b44cbd9e07abb4947
josedelinux <josedelinux@hotmail.com>
2021-12-30 12:12:01 +0800

    Update main.cpp

--- p/1k/1047/main.cpp
+++ p/1k/1047/main.cpp
@@ -1,31 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define maxn 10005
 
 char arr[maxn];
 
-int main()
-{
-	int ans=0;
-	//init to 0
-	memset(arr,0,maxn);
-	int l,m;
-	scanf("%d",&l);
-	memset(arr,1,l);
+int main() {
+  int ans = 0;
+  memset(arr, 0, maxn);
+  int l, m;
+  scanf("%d", &l);
+  memset(arr, 1, l + 1);
 
-	scanf("%d",&m);
-	while(m--) {
-		int a,b;
-		scanf("%d %d",&a,&b);
-		for(int i=a; i<=b; i++) {
-			arr[i]=0;
-		}
-	}
-	
-	
-	for(int i=0; i<=l; i++) {
-		if(arr[i]) ans++;
-	}
-	printf("%d\n",ans);
-	return 0;
+  scanf("%d", &m);
+  while (m--) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    for (int i = a; i <= b; i++) {
+      arr[i] = 0;
+    }
+  }
+
+  for (int i = 0; i <= l; i++) {
+    if (arr[i]) ans++;
+  }
+  printf("%d\n", ans);
+  return 0;
 }
