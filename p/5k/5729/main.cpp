54203922ec1e03f0dc15706871ad27066eb668b1
josedelinux <josedelinux@hotmail.com>
2021-12-31 23:13:31 +0800

    Update main.cpp

--- p/5k/5729/main.cpp
+++ p/5k/5729/main.cpp
@@ -1,23 +1,30 @@
+/*
+终究是数组题单下面的题,肯定要用数组的...
+*/
 #include <bits/stdc++.h>
 using namespace std;
 #define maxn 1005
 
+int cub[21][21][21] = {0};
 
-int main()
-{
-	int w,x,h;
-	int q;
-	scanf("%d%d%d",&w,&x,&h);
-	int ans = w*x*h;
-	scanf("%d",&q);
-	while(q--) {
-		int x1,y1,z1;
-		int x2,y2,z2;
-		scanf("%d %d %d %d %d %d",&x1,&y1,&z1,&x2,&y2,&z2);
-		int deduction= (abs(x1-x2) +1)*(abs(y1-y2) +1)*(abs(z1-z2) +1);
-		ans=ans -deduction;
-	}
-	printf("%d\n", ans);
-	return 0;
+int main() {
+  int w, x, h;
+  int q;
+  scanf("%d%d%d", &w, &x, &h);
+  int ans = w * x * h;
+  scanf("%d", &q);
+  while (q--) {
+    int x1, y1, z1;
+    int x2, y2, z2;
+    scanf("%d %d %d %d %d %d", &x1, &y1, &z1, &x2, &y2, &z2);
+    for (int i = min(x1, x2); i <= max(x1, x2); i++) {
+      for (int j = min(y1, y2); j <= max(y1, y2); j++) {
+        for (int k = min(z1, z2); k <= max(z1, z2); k++) {
+          cub[i][j][k] = 1;
+        }
+      }
+    }
+  }
+  printf("%d\n", ans);
+  return 0;
 }
-
