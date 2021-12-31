af487c34da712e880945b2f384947f9c2e1b996a
josedelinux <josedelinux@hotmail.com>
2021-12-31 22:57:44 +0800

    Update main.cpp

--- p/5k/5729/main.cpp
+++ p/5k/5729/main.cpp
@@ -10,12 +10,12 @@ int main()
 	scanf("%d%d%d",&w,&x,&h);
 	int ans = w*x*h;
 	scanf("%d",&q);
-	for(q--) {
+	while(q--) {
 		int x1,y1,z1;
 		int x2,y2,z2;
 		scanf("%d %d %d %d %d %d",&x1,&y1,&z1,&x2,&y2,&z2);
 		int deduction= (abs(x1-x2) +1)*(abs(y1-y2) +1)*(abs(z1-z2) +1);
-		ans=ans deduction;
+		ans=ans -deduction;
 	}
 	printf("%d\n", ans);
 	return 0;
