7bd6c170cbd0dcbb5ef8873f9ad94583f3430aa3
josedelinux <josedelinux@hotmail.com>
2022-01-08 12:52:03 +0800

    Create main.cpp

---
+++ p/2k/2911/main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+int main(){
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+	
+	for(int i=0;i<a;i++){
+		for (int j=0;j<b;j++){
+			for (int k =0;k<c;k++){
+				printf("%d %d %d\n",i,j,k);
+			}
+		}
+	}
+	return 0;
+}
