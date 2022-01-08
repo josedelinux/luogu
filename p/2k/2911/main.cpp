fd2d5586b557699c6914e9c3a877fd531260e28d
josedelinux <josedelinux@hotmail.com>
2022-01-08 13:07:16 +0800

    Update main.cpp

--- p/2k/2911/main.cpp
+++ p/2k/2911/main.cpp
@@ -1,15 +1,30 @@
-#include<bits/stdc++.h>
+#include<stdio.h>
 
-int main(){
+int main()
+{
 	int a,b,c;
+	int knt[20]= {0};
+	int max=0;
+	int ans;
 	scanf("%d%d%d",&a,&b,&c);
-	
-	for(int i=0;i<a;i++){
-		for (int j=0;j<b;j++){
-			for (int k =0;k<c;k++){
-				printf("%d %d %d\n",i,j,k);
+
+	for(int i=1; i<=a; i++) {
+		for (int j=1; j<=b; j++) {
+			for (int k =1; k<=c; k++) {
+#ifdef DEBUG
+				printf("%d %d %d -> %d\n",i,j,k,i+j+k);
+#endif
+				knt[i+j+k]++;
 			}
 		}
 	}
+	//from the back, we will get the first biggest one
+	for(int i=20; i>=0; i--) {
+		if(knt[i]>=max)	{
+			max =knt[i];
+			ans = i;
+		}
+	}
+	printf("%d\n",ans);
 	return 0;
 }
