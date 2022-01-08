f460a139e1eeb1168b4650c0ae285b075f538e0b
josedelinux <josedelinux@hotmail.com>
2022-01-08 13:20:53 +0800

    Update main.cpp

--- p/2k/2911/main.cpp
+++ p/2k/2911/main.cpp
@@ -1,30 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-	int a,b,c;
-	int knt[20]= {0};
-	int max=0;
-	int ans;
-	scanf("%d%d%d",&a,&b,&c);
+#define maxn 255
 
-	for(int i=1; i<=a; i++) {
-		for (int j=1; j<=b; j++) {
-			for (int k =1; k<=c; k++) {
+int main() {
+  int a, b, c;
+  int knt[maxn] = {0};
+  int max = 0;
+  int ans;
+  scanf("%d%d%d", &a, &b, &c);
+
+  for (int i = 1; i <= a; i++) {
+    for (int j = 1; j <= b; j++) {
+      for (int k = 1; k <= c; k++) {
+        int sum = i + j + k;
 #ifdef DEBUG
-				printf("%d %d %d -> %d\n",i,j,k,i+j+k);
+        printf("%d %d %d -> %d\n", i, j, k, sum);
 #endif
-				knt[i+j+k]++;
-			}
-		}
-	}
-	//from the back, we will get the first biggest one
-	for(int i=20; i>=0; i--) {
-		if(knt[i]>=max)	{
-			max =knt[i];
-			ans = i;
-		}
-	}
-	printf("%d\n",ans);
-	return 0;
+        knt[sum]++;
+      }
+    }
+  }
+
+  // from the back, we will get the first biggest one
+  for (int i = maxn; i >= 0; i--) {
+    if (knt[i] >= max) {
+      max = knt[i];
+      ans = i;
+    }
+  }
+  printf("%d\n", ans);
+  return 0;
 }
