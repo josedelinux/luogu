14e5c9bbe19905273b0b8c2d6ffc65b00818b83a
josedelinux <josedelinux@hotmail.com>
2022-01-11 14:00:57 +0800

    Update main.cpp

--- p/2k/2141/main.cpp
+++ p/2k/2141/main.cpp
@@ -4,18 +4,20 @@ int main() {
   int n = 0;
   int cnt = 0;
   int arr[105] = {0};
+  bool checked[105] = {false};
 
   scanf("%d", &n);
 
   for (int i = 0; i < n; i++) {
     scanf("%d", &arr[i]);
   }
-  // std::sort(arr, arr + n);
+
   for (int i = 0; i < n; i++) {
     for (int j = i + 1; j < n; j++) {
-      for (int k = j + 1; k < n; k++) {
-        if (arr[i] + arr[j] == arr[k]) {
+      for (int k = 0; k < n; k++) {
+        if (!checked[k] && arr[i] + arr[j] == arr[k]) {
           cnt++;
+          checked[k] = true;
         }
       }
     }
