a6dda2f9195c7338b971d64cb640927d80d3308e
josedelinux <josedelinux@hotmail.com>
2022-01-11 12:43:31 +0800

    Update main.cpp

--- p/2k/2141/main.cpp
+++ p/2k/2141/main.cpp
@@ -3,18 +3,20 @@
 int main() {
   int n = 0;
   int cnt = 0;
-  int arr[105];
+  int arr[105] = {0};
 
   scanf("%d", &n);
 
   for (int i = 0; i < n; i++) {
-    scanf("%d", arr[i]);
+    scanf("%d", &arr[i]);
   }
   // std::sort(arr, arr + n);
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; i++) {
+    for (int j = 0; j < n; j++) {
       for (int k = 0; k < n; k++) {
-        if (arr[i] + arr[j] == arr[k]) cnt++;
+        if (arr[i] + arr[j] == arr[k]) {
+          cnt++;
+        }
       }
     }
   }
