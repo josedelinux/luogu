209534720b5738fd34c2b1af14953c90fca5bdc7
josedelinux <josedelinux@hotmail.com>
2022-01-11 12:50:05 +0800

    Update main.cpp

--- p/2k/2141/main.cpp
+++ p/2k/2141/main.cpp
@@ -12,8 +12,8 @@ int main() {
   }
   // std::sort(arr, arr + n);
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      for (int k = 0; k < n; k++) {
+    for (int j = i + 1; j < n; j++) {
+      for (int k = j + 1; k < n; k++) {
         if (arr[i] + arr[j] == arr[k]) {
           cnt++;
         }
