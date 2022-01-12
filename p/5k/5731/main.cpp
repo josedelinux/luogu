170e0b3c50769129962d70213aa23564f870b153
josedelinux <josedelinux@hotmail.com>
2022-01-12 23:03:28 +0800

    Update main.cpp

--- p/5k/5731/main.cpp
+++ p/5k/5731/main.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
+#define maxn 10000
 using namespace std;
+
+int arr[maxn][maxn];
+
+int pos[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
 int main() {
   int n;
   scanf("%d", &n);
   int kmax = n * n;
-  int cnt = 0;
+  int cnt = 1;
 
+  /* output */
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       printf("%3d", cnt++);
