e418ab1645f1b3e145cd853193d1fcabbad34cae
josedelinux <josedelinux@hotmail.com>
2022-01-13 20:01:07 +0800

    Create main.cpp

---
+++ p/5k/5732/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// nCr {n \choose x} = n!/r!*(n-r)!
+long long C(int n, int r) {
+  if (r > n - r) r = n - r;
+  long long ans = 1;
+  for (int i = n; i > n - r; i--) {
+    ans = ans * i;
+  }
+  for (int i = r; i > 1; i--) {
+    ans = ans / i;
+  }
+  return ans;
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  for (int row = 1; row <= n; row++) {
+    for (int col = 1; col <= row; col++) {
+      printf("%d ", C(row, col));
+    }
+    putchar('\n');
+  }
+  return 0;
+}
