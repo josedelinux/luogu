041ad8cd4cca6cb190a444e5dd229c9a98acddba
josedelinux <josedelinux@hotmail.com>
2022-01-24 21:26:54 +0800

    Update main.cpp

--- p/5k/5734/main.cpp
+++ p/5k/5734/main.cpp
@@ -1,15 +1,25 @@
 #include <cstdio>
+#include <cstring>
 
 int n;
 char s[101];
 char str[101];
-int k;
+int k;  // command
 
 int main() {
   scanf("%d", &n);
   scanf("%s", &s);
   for (int i = 0; i <= n; i++) {
     scanf("%d %s", &k, &str);
+    switch (k) {
+      case 1:
+        // copy
+        strcat(s, str);
+        break;
+      default:
+        break;
+    }
+    printf("%s", s);
   }
 
   return 0;
