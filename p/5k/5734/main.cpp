54872d17f80e15ff83b1c5869607ce21cded7894
josedelinux <josedelinux@hotmail.com>
2022-01-24 21:22:30 +0800

    Create main.cpp

---
+++ p/5k/5734/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int n;
+char s[101];
+char str[101];
+int k;
+
+int main() {
+  scanf("%d", &n);
+  scanf("%s", &s);
+  for (int i = 0; i <= n; i++) {
+    scanf("%d %s", &k, &str);
+  }
+
+  return 0;
+}
