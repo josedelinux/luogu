34edbfc671b31adc1331ba8fef6b1474e499fcc7
josedelinux <josedelinux@hotmail.com>
2022-01-24 21:38:50 +0800

    Update main.cpp

--- p/5k/5734/main.cpp
+++ p/5k/5734/main.cpp
@@ -10,17 +10,23 @@ int main() {
   scanf("%d", &n);
   scanf("%s", &s);
   for (int i = 0; i < n; i++) {
-    scanf("%d %s", &k, &str);
+    scanf("%d", &k);
     switch (k) {
       case 1:
-        // copy
+        // catenate to the end
+        scanf("%s", &str);
         strcat(s, str);
         break;
       case 2:
         // cut b letter from pos a
-
-        break;
-      default:
+        int a, b;
+        scanf("%d %d", &a, &b);
+        for (int j = a; j < b + a; j++) {
+          putchar(s[j]);
+        }
+        continue;
+      case 3:
+        // insert
         break;
     }
     printf("%s\n", s);
