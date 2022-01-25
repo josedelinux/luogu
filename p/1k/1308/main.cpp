bd0d137cc3cd0c5bfd2d439f83ed2a03facd5e24
josedelinux <josedelinux@hotmail.com>
2022-01-25 13:42:20 +0800

    Create main.cpp

---
+++ p/1k/1308/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string substr;
+  string str;
+
+  char* pos = NULL;  // first location
+  int vez = 0;       // times
+
+  // hail getline
+  getline(cin, substr);
+  getline(cin, str);
+
+  for (int i = 0; i < substr.length(); i++) substr[i] = toupper(substr[i]);
+  for (int i = 0; i < str.length(); i++) str[i] = toupper(str[i]);
+
+  substr = ' ' + substr + ' ';
+  str = ' ' + str + ' ';  // keep the count same since substr added
+
+  if (str.find(substr) == string::npos) {
+    cout << -1 << endl;
+  } else {
+    int cnt = 0;
+    int first = str.find(substr);
+    int tmp = first;
+    while (tmp != string::npos) {
+      cnt++;
+      tmp = str.find(substr, tmp + 1);
+    }
+    cout << cnt << " " << first << endl;
+  }
+
+  return 0;
+}
