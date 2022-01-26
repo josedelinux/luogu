3f1d17d82ccacf679db844ea0884c909a9e01d39
josedelinux <josedelinux@hotmail.com>
2022-01-26 19:51:44 +0800

    Update main.cpp

--- p/3k/3741/main.cpp
+++ p/3k/3741/main.cpp
@@ -3,7 +3,7 @@
 
 vv(ok)
 kk(ok)
-vk(ok)
+vk(ok,target)
 kv(unchangeable)
 
 */
@@ -13,6 +13,7 @@ using namespace std;
 
 int main() {
   int cnt = 0;
+  int flag = 0;  // "yes we can change" flag
   int n;
   string s;
   cin >> n;
@@ -23,11 +24,23 @@ int main() {
 
     if (cur == 'K' && next == 'V') {
       // ignore
-    } else if (isalpha(cur) && isalpha(next)) {
+      continue;
+    } else if (cur == 'V' && next == 'K') {
       cnt++;
+    } else if (!flag && isalpha(cur) && isalpha(next)) {
+      /* 2 exception: VKVVK VKKVK
+       */
+      if (s[i - 1] == 'K' && s[i - 2] == 'V' && s[i + 1] == 'V' &&
+          s[i + 2] == 'K') {
+        // hace nada
+      } else if (s[i - 1] == 'V' && cur == 'K') {
+        // hace nada
+      } else {
+        flag = 1;
+      }
     }
   }
 
-  cout << cnt << endl;
+  cout << cnt + flag << endl;
   return 0;
 }
