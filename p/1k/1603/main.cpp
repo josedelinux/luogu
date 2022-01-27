b92f9edbcc81ba8bf4c00c294a12506c93e85501
josedelinux <josedelinux@hotmail.com>
2022-01-27 12:32:16 +0800

    Create main.cpp

---
+++ p/1k/1603/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+map<string, int> m;
+
+void init() {
+  m["one"] = 1;
+  m["two"] = 2;
+  m["three"] = 3;
+  m["four"] = 4;
+  m["five"] = 5;
+  m["six"] = 6;
+  m["seven"] = 7;
+  m["eight"] = 8;
+  m["nine"] = 9;
+  m["ten"] = 10;
+  m["eleven"] = 11;
+  m["twelve"] = 12;
+  m["thirteen"] = 13;
+  m["fourteen"] = 14;
+  m["fifteen"] = 15;
+  m["sixteen"] = 16;
+  m["seventeen"] = 17;
+  m["eighteen"] = 18;
+  m["nineteen"] = 19;
+  m["twenty"] = 20;
+  m["a"] = 1;
+  m["both"] = 2;
+  m["another"] = 1;
+  m["first"] = 1;
+  m["second"] = 2;
+  m["third"] = 3;
+}
+
+int main() { return 0; }
