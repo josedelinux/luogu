f8ed355d92e1af2dc26d246fc7096245d08012f7
josedelinux <josedelinux@hotmail.com>
2022-02-09 13:37:07 +0800

    Update main.cpp

--- p/1k/1157/main.cpp
+++ p/1k/1157/main.cpp
@@ -1,42 +1,50 @@
 /*
 I wrote it my self
 hail brute force
+slow as fuck
+O(n!) max: 20!
 */
 #include <algorithm>
-#include <iostream>
+#include <cstdio>
+#include <cstring>
 
-using namespace std;
+const int maxn = 100;
 
 int main() {
   int arr[20] = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
                  11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
   int n, r;
-  cin >> n >> r;
-  string last = "";
+
+  char last[maxn] = "";
+
+  scanf("%d %d", &n, &r);
+
   do {
     bool skip = false;
     // check if the latter is greater than before, if so, skip
-    for (int i = 0; i < r - 1; i++) {
+    for (int i = 0; !skip && i < r - 1; i++) {
       if (arr[i] > arr[i + 1]) {
         skip = true;
       }
     }
 
-    string cur = "";
+    char cur[maxn] = "";
+    char* pcur = cur;
+    int offset = 0;
     if (!skip && 1) {
       for (int i = 0; i < r; i++) {
-        // cout << "  " << arr[i];
-        cur = cur + "  " + (char)(arr[i] + '0');
+        sprintf(pcur, "%3d%n", arr[i], &offset);
+        pcur += offset;
       }
-      // cout << endl;
-      cur = cur + '\n';
     }
 
-    if (last != cur) {
-      cout << cur;
+    if (strcmp(cur, last) != 0 && strcmp(cur, "") != 0) {
+      puts(cur);
     }
-    last = cur;
-  } while (next_permutation(arr, arr + n));
+
+    strcpy(last, cur);
+
+  } while (std::next_permutation(arr, arr + n));
 
   return 0;
 }
