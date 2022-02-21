638ce85f2393e247f1c533bc88d70471dcd8bb91
josedelinux <josedelinux@hotmail.com>
2022-02-21 19:12:53 +0800

    Update main.cpp

--- p/5k/5736/main.cpp
+++ p/5k/5736/main.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-char is_prime[MAX_N + 1];  // boolean array
+bool is_prime[MAX_N];  // boolean array
 
 void primeGen(int n) {
   memset(is_prime, 1, sizeof(is_prime));
-  is_prime[0] = is_prime[1] = 0;
+  is_prime[0] = is_prime[1] = false;
   for (int i = 2; i <= n; i++) {
     if (is_prime[i]) {
       for (int j = 2 * i; j <= n; j += i) {
-        is_prime[j] = 0;
+        is_prime[j] = false;
       }
     }
   }
